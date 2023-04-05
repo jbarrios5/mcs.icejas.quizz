e389818be4f345422d39f1373c19f10f599b6121
juan <marcecinco@fpuna.edu.py>
2023-04-04 21:06:03 -0400

    Added password encrypt with spring security, and we'll send default localDateTIme in createAt

--- src/main/java/py/com/icejas/quizz/user/UserServieImpl.java
+++ src/main/java/py/com/icejas/quizz/user/UserServieImpl.java
@@ -3,10 +3,14 @@ package py.com.icejas.quizz.user;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import py.com.icejas.quizz.constants.ApiError;
 import py.com.icejas.quizz.exception.ApiRequestException;
 
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -15,13 +19,18 @@ public class UserServieImpl implements UserService{
     private static final Logger log = LoggerFactory.getLogger(UserServieImpl.class);
     @Autowired
     UserDAO userDAO;
+    @Autowired
+    PasswordEncoder passwordEncoder;
     @Override
     public void insertUser(User user) {
         log.info("We've received {}",user);
-        //todo encrypt password
-
         if(user == null)
             throw new ApiRequestException("User is required "+ ApiError.BAD_REQUEST_DATA);
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setCreateAt(Timestamp.valueOf(LocalDateTime.now()));
+
+
 
         userDAO.insertUser(user);
     }
